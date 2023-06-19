a21ce11b9032268d9f2e138d345e3410bad862a3
CoderAashishXD <98446454+CoderAashishXD@users.noreply.github.com>
2023-06-19 11:08:34 +0530

    Before OOP

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int qNo = 0;
+  List<bool> answers = [false, true, true];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[qNo],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,7 +68,22 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[qNo];
+                if (correctAnswer == true) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
                 //The user picked true.
+                setState(() {
+                  qNo++;
+                });
               },
             ),
           ),
@@ -80,7 +101,22 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answers[qNo];
+                if (correctAnswer == false) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
                 //The user picked false.
+                setState(() {
+                  qNo++;
+                });
               },
             ),
           ),
